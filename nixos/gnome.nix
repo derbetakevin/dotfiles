a8009fe32ffcf5395396bd92c73da65663e8db4f
Der Beta Kevin <derbetakevin@outlook.de>
2024-01-21 14:19:53 +0100

    Add gnome.nix

---
+++ nixos/gnome.nix
@@ -0,0 +1,45 @@
+{ config, pkgs, ... }: 
+  
+{
+  config = {
+    services = {
+      xserver = {
+        enable = true;
+        displayManager.gdm = {
+          enable = true; 
+        };
+
+        desktopManager.gnome = {
+          enable = true;
+        };
+      };
+
+      fwupd = {
+        enable = true;
+      };
+    };
+
+    # XDG Desktop Portal
+    xdg = {
+      portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-gnome
+        ];
+      };
+    };
+
+    environment = {
+      systemPackages = with pkgs; [
+        gnome.gnome-tweaks
+        gnomeExtensions.appindicator
+        gnomeExtensions.blur-my-shell
+        gnomeExtensions.dash-to-dock
+        gnomeExtensions.just-perfection
+        gnomeExtensions.removable-drive-menu
+        gnomeExtensions.vitals
+        yaru-theme
+      ];
+    };
+  };
+}
