f5b6987d381ac9b02a40e40f9208647c981e754e
Der Beta Kevin <derbetakevin@outlook.de>
2024-02-01 01:11:14 +0100

    Add various things

---
+++ configurations/waybar/kernel-widget-function.sh
@@ -0,0 +1,6 @@
+#Fork of https://github.com/JerrySM64/dotfiles/blob/556d1add7a798ced9ef7c44bedd4202075c7e19c/dots/.config/qtile/scripts/kernel-widget-function.sh
+#!/usr/bin/env bash
+
+neofetch
+echo "Press ENTER key..."
+read
